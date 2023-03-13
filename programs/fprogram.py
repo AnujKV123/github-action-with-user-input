4ae97355250e1cefdb5872877ea8b3dc9742bdae
Anuj <anuj8ranchi@gmail.com>
2023-03-13 23:45:26 +0530

    program with input values

--- programs/fprogram.py
+++ programs/fprogram.py
@@ -7,7 +7,7 @@ def add(a, b):
 
 a = int(sys.argv[0])
 b = int(sys.argv[1])
-c = sys.argv[2]
+# c = sys.argv[2]
 
 print(f'sum to {a} and {b} = {add(a, b)}')
-print("Hello ", c)
+# print("Hello ", c)
