37b6ce49673e6788476d382efadc504cf223cb8d
Anuj <anuj8ranchi@gmail.com>
2023-03-13 22:48:34 +0530

    Initial commit just for testing

---
+++ programs/fprogram.py
@@ -0,0 +1,10 @@
+# def add(a, b):
+#     x = a+b
+#     return x
+
+# a = int(input("Enter the value of a: "))
+# b = int(input("Enter the value of b: "))
+
+# print(f'sum to {a} and {b} = {add(a, b)}')
+
+print("Hello World")
